916f36c9d9e693a25b632be66c453f475723c3a3
harwindersinghgill <harwindersinghgill37@gmail.com>
2025-08-12 16:36:21 +0800

    refactor: Simplify auth for multi-platform test

--- lib/data-fetching.ts
+++ lib/data-fetching.ts
@@ -1,16 +1,13 @@
 // lib/data-fetching.ts
 import 'server-only';
+const base64 = require('base-64');
 
 function getAuthHeaders() {
   const WP_USER = process.env.WP_USER ?? '';
   const WP_PASSWORD = process.env.WP_PASSWORD ?? '';
-  const credentials = `${WP_USER}:${WP_PASSWORD}`;
-  // Use the built-in btoa function, which is Edge-runtime compatible
-  const token = btoa(credentials);
 
-  // We ONLY need the WordPress Authorization now.
   return {
-    'Authorization': `Basic ${token}`,
+    'Authorization': 'Basic ' + base64.encode(`${WP_USER}:${WP_PASSWORD}`),
     'Content-Type': 'application/json'
   };
 }
